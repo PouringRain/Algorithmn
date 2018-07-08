8837065bf4e274a5c08c575c04ec1a8dd5678b10
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-07-08 21:46:10 +0800

    一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。

--- 6.py
+++ 6.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 class Solution:
-    def jumpFloorII(self, number):
+    def jumpFloor(self, number):
         # write code here
-        if number==1 or number==0:
-            return 1
-        if number==2: 
-            return 2
-        ans = 0
-        for i in range(number):
-            ans+=self.jumpFloorII(i)
-        return ans
+        if number==1 or number==2:
+            return number
+        # dp(n) = dp(n-1)+dp(n-2)
+        dp = [1, 2]
+        for i in range(2, number):
+            dp.append(dp[i-1]+dp[i-2])
+        
+        return dp[-1]
