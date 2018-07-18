d95986b20596f0e6936da0b6a1ea153697bb5a2a
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-07-18 10:14:04 +0800

    给定一个数组和滑动窗口的大小，找出所有滑动窗口里数值的最大值

--- 27.py
+++ 27.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+# 最low解法
 class Solution:
     def maxInWindows(self, num, size):
         # write code here
@@ -9,3 +10,29 @@ class Solution:
             res.append(max(num[i:i+size]))
         
         return res
+    
+# solution 2
+# -*- coding:utf-8 -*-
+import collections
+class Solution:
+    def maxInWindows(self, num, size):
+        # write code here
+        # solution: 双向队列
+        # 1. num[i]比前面的大，前面的都出队
+        # 2. num[i]比前面的小，入队。若入队后len(滑窗)>size,队首出队
+        q = collections.deque()
+        ret = []
+        for i, val in enumerate(num):
+            print(q)
+
+            if q:
+                if len(q) >= size:
+                    q.popleft()
+                while q and q[0] <= val:
+                    q.pop()
+
+            q.append(val)
+            if i+1 >= size:
+                ret.append(q[0])
+
+        return ret
