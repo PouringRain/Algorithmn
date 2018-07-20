e2e1b99dfc2140577fc316e05696364cf47cbfd1
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-07-20 11:01:15 +0800

    *把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转

--- 29.py
+++ 29.py
@@ -9,3 +9,23 @@ class Solution:
                 return rotateArray[i+1]
         return rotateArray[0]
             
+        
+ # 稍微快一点的方法： 551ms
+ # -*- coding:utf-8 -*-
+class Solution:
+    def minNumberInRotateArray(self, rotateArray):
+        # write code here
+        # 二分查找
+        lenth = len(rotateArray)
+        if lenth == 0:
+            return 0
+        left, right = 0, lenth - 1
+        while left <= right:
+            mid = (left+right)/ 2
+            if rotateArray[mid] < rotateArray[mid-1]:
+                return rotateArray[mid]
+            elif rotateArray[mid] < rotateArray[left]:
+                right = mid
+            else:
+                left = mid
+        #return rotateArray[0]
