4b3b297531406d79c0b78849e53cedd4a181b0b4
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-07-20 20:06:40 +0800

    Update 25.py

--- 25.py
+++ 25.py
@@ -21,3 +21,29 @@ class Solution:
         # 中序遍历
         self.inTravel(pRoot, k)
         return self.ret
+
+    
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回对应节点TreeNode
+    def __init__(self):
+        self.count = 0
+    def KthNode(self, pRoot, k):
+        # write code here
+        if pRoot: 
+            
+            node = self.KthNode(pRoot.left, k)
+            if node: return node
+            self.count += 1
+            if k == self.count:
+                return pRoot
+            node = self.KthNode(pRoot.right, k)
+            if node: return node
+        return None
+    
+  # 非递归
