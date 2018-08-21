afff574a6ce0b6e402707f628a583877222043ce
盛剑 <32264988+jsheng2017@users.noreply.github.com>
2018-08-21 21:26:21 +0800

    数值的n次方

--- 60.py
+++ 60.py
@@ -8,3 +8,20 @@ class Solution:
         for i in range(abs(exponent)):
             ret *= base
         return ret if exponent>0 else 1/ret
+    
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, n):
+        # write code here
+        ret, exp = 1, -1
+        cur = base # 当前
+        if n>0: exp = n
+        elif n==0: return 1
+        else: exp = -n
+        while exp:
+            if exp&1==1: 
+                ret *= cur
+            cur *= cur
+            exp>>=1
+        return ret if n>0 else 1/ret
+        
